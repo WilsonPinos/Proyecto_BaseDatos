efab572dd8c6ebf05fd9fef5b6b6eb069fa5ab54
mauri <mauri@DESKTOP-RB6N7H7.lan>
2023-07-08 16:23:29 -0500

    clase Persona

---
+++ src/proyecto_basedatos/Persona.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto_basedatos;
+
+import java.util.Date;
+
+/**
+ *
+ * @author mauri
+ */
+public class Persona {
+
+    private String nombre, apellido, edad, celular, correo;
+    private Date nacimiento;
+
+    public Persona() {
+    }
+    
+    
+
+    public Persona(String nombre, String apellido, String edad, String celular, String correo, Date nacimiento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.celular = celular;
+        this.correo = correo;
+        this.nacimiento = nacimiento;
+    }
+    
+    
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the edad
+     */
+    public String getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    /**
+     * @return the celular
+     */
+    public String getCelular() {
+        return celular;
+    }
+
+    /**
+     * @param celular the celular to set
+     */
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    /**
+     * @return the correo
+     */
+    public String getCorreo() {
+        return correo;
+    }
+
+    /**
+     * @param correo the correo to set
+     */
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    /**
+     * @return the nacimiento
+     */
+    public Date getNacimiento() {
+        return nacimiento;
+    }
+
+    /**
+     * @param nacimiento the nacimiento to set
+     */
+    public void setNacimiento(Date nacimiento) {
+        this.nacimiento = nacimiento;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad + ", celular=" + celular + ", correo=" + correo + ", nacimiento=" + nacimiento + '}';
+    }
+
+}
